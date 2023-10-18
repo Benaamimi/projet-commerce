246416d89b7d04fc968c98249c1acdca8532a16c
Tarik_B <tarik_b@MBP-de-Loubna.lan>
2023-10-18 13:02:01 +0200

    ajout du composant mailer

--- src/EventDispatcher/ProductViewEmailSubscriber.php
+++ src/EventDispatcher/ProductViewEmailSubscriber.php
@@ -4,15 +4,21 @@ namespace App\EventDispatcher;
 
 use App\Event\ProductViewEvent;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 
 class ProductViewEmailSubscriber implements EventSubscriberInterface
 {
     protected $logger;
+    protected $mailer;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer)
     {
         $this->logger =$logger;
+        $this->mailer =$mailer;
     }
 
     public static function getSubscribedEvents()
@@ -24,6 +30,18 @@ class ProductViewEmailSubscriber implements EventSubscriberInterface
 
     public function sendEmail(ProductViewEvent $productViewEvent)
     {
+        // $email = new TemplatedEmail();
+        // $email->from(new Address("contact@gmail.com", "Info de la boutique"))
+        // ->to("admin@gmail.com")
+        // ->text("Un visiteur est en train de voir la page du produit n°" . $productViewEvent->getProduct()->getId())
+        // ->htmlTemplate('emails/product_view.html.twig')
+        // ->context([
+        //     'product' => $productViewEvent->getProduct() //* cibler le produit concerné (twig)
+        // ])
+        // ->subject("Visite du produit n°" . $productViewEvent->getProduct()->getId());
+
+        // $this->mailer->send($email);
+
         $this->logger->info("Email envoyé pour le produit n°" . $productViewEvent->getProduct()->getId());
     } 
 }
